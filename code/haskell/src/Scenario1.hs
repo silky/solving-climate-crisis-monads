b5df2386d98d47acce0cd3ddaf92e42bcf173bfb
silky <noonsilk+-noonsilk@gmail.com>
2022-11-07 08:47:56 +0000

    wip

--- code/haskell/src/Scenario1.hs
+++ code/haskell/src/Scenario1.hs
@@ -1,8 +1,38 @@
+{-# language GADTs #-}
+{-# language FlexibleInstances #-}
+
 module Scenario1 where
 
-data World = World
+data World a b = World
   { resources :: Int
+  -- , entities  :: [Business a b]
   }
 
-business :: a -> b
-business = undefined
+-- liftBusiness :: a -> Business a b -> Business (World () ()) (World () ())
+-- liftBusiness a b = undefined
+--   where
+--     b a
+
+data Runnable = forall a b. Business a b => MkRunnable a b
+
+class Business a b where
+  run :: a -> IO b
+
+
+instance Business a (a -> b) where
+  run = undefined
+
+data Plants
+data Flowers
+
+-- florist :: Business Plants Flowers
+-- florist = undefined
+
+
+
+data Beans
+data Milk   = Oat
+data Coffee = Latte | Cappuccino
+
+-- cafe :: Business (Beans, Milk) Coffee
+-- cafe = undefined
