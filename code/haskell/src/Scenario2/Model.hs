64aa52c6fa30f055574bd21860adc43bf04be025
silky <noonsilk+-noonsilk@gmail.com>
2022-11-17 19:13:19 +0000

    getting there

--- code/haskell/src/Scenario2/Model.hs
+++ code/haskell/src/Scenario2/Model.hs
@@ -1,42 +1,38 @@
+{-# language AllowAmbiguousTypes #-}
+
 module Scenario2.Model where
 
 import "base" Data.Monoid (Sum (..))
-import "base" Data.Proxy (Proxy (..))
 import Scenario2.Types
-
-data Plants  = Plant
-data Flowers = Flower
 
 safeToConsume :: Sum Integer -> World -> Bool
 safeToConsume cost' World{resources} = resources - cost' > 70
 
-produce
+safelyProduce
   :: forall p v
    . Cost (p -> WorldState v)
-  => Proxy p
-  -> v
+  => v
   -> WorldState v
-produce _ v = WorldState $ \w ->
+safelyProduce v = WorldState $ \w ->
   if safeToConsume (cost @(p -> WorldState v)) w
      then (Just v,  w)
      else (Nothing, w)
 
+data Plants  = Plant
+data Flowers = Flower
+
 florist :: Plants -> WorldState Flowers
-florist = const $ produce (undefined :: Proxy Plants) Flower
+florist = const $ safelyProduce @Plants @_ Flower
 
 instance Cost (Plants -> WorldState Flowers) where
   cost = Sum 1
 
--- instance Cost ((Beans, Milk) -> WorldState Coffee) where
---   cost = Sum 1
+data Beans  = Bean
+data Milk   = Oat
+data Coffee = Latte | Cappuccino
 
--- instance Default Plants where
---   def = Plant
+cafe :: (Beans, Milk) -> WorldState Coffee
+cafe = const $ safelyProduce @(Beans, Milk) @_ Latte
 
-
--- data Beans  = Bean
--- data Milk   = Oat
--- data Coffee = Latte | Cappuccino
-
--- cafe :: (Beans, Milk) -> WorldState Coffee
--- cafe = const $ produce Latte
+instance Cost ((Beans, Milk) -> WorldState Coffee) where
+  cost = Sum 1
